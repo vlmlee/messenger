39cf90b3d133ee5866d7cb87dc891fff693657d9
Michael Lee <vlmlee2@gmail.com>
2022-11-06 20:07:20 -0600

    Imported Modules to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MessageModule } from './messages/messages.module';
+import { UserModule } from './users/users.module';
 
 @Module({
     imports: [
+        MessageModule,
+        UserModule,
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver
         })
