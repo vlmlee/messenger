0d749e5343e79599bec4d5f71585708325f9a031
Michael Lee <vlmlee2@gmail.com>
2022-11-06 20:23:44 -0600

    Fixed ApolloDriverConfig.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,26 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MessageModule } from './messages/messages.module';
 import { UserModule } from './users/users.module';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
+import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
 
 @Module({
     imports: [
         MessageModule,
         UserModule,
         GraphQLModule.forRoot<ApolloDriverConfig>({
-            driver: ApolloDriver
+            driver: ApolloDriver,
+            autoSchemaFile: 'schema.gql',
+            transformSchema: schema => upperDirectiveTransformer(schema, 'upper'),
+            installSubscriptionHandlers: true,
+            buildSchemaOptions: {
+                directives: [
+                    new GraphQLDirective({
+                        name: 'upper',
+                        locations: [DirectiveLocation.FIELD_DEFINITION]
+                    })
+                ]
+            }
         })
     ],
     controllers: [AppController],
