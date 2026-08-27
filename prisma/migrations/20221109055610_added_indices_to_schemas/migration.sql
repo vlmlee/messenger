c4334e2697d5ec7df2e7bd53b75348bc0d56874f
Michael Lee <vlmlee2@gmail.com>
2026-08-26 22:31:29 -0500

    delete migration files to start from scratch

--- prisma/migrations/20221109055610_added_indices_to_schemas/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "Friend_id_idx" ON "Friend"("id");
-
--- CreateIndex
-CREATE INDEX "Message_id_createdAt_idx" ON "Message"("id", "createdAt" DESC);
-
--- CreateIndex
-CREATE INDEX "User_id_createdAt_idx" ON "User"("id", "createdAt" DESC);
