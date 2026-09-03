4955b09abe8d0c13b5182d03b84d4d39152fc4de
Michael Lee <vlmlee2@gmail.com>
2026-09-03 02:25:15 -0500

    update to pip boy colors

--- client/src/components/Channel.tsx
+++ client/src/components/Channel.tsx
@@ -11,8 +11,8 @@ interface IChannelComponent extends IChannel {
 
 const Channel = ({ id, createdBy, participants, selectChannel }: IChannelComponent) => (
     <div className={'channel'} onClick={() => selectChannel(id)}>
-        <div>Channel id: &#91;{id}&#93;</div>
-        <div className={'channel__user'}> Created by: &#91;{createdBy}&#93; (You)</div>
+        <div>Channel id &#91;{id}&#93;</div>
+        <div className={'channel__user'}> Created by &#91;{createdBy}&#93; (You)</div>
         <div className={'channel__friend'}> Participants: {participants?.map((p: string) => <p key={p}> &gt; &#91;{p}&#93;</p>)}</div>
     </div>
 );
